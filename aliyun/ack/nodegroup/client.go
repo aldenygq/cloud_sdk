235e0111055049ec07c51e2378a5645721f568fa
夏沫 <summer@xiamodeMacBook-Air.local>
2024-08-07 23:03:21 +0800

    add nodegroup

--- aliyun/ack/nodegroup/client.go
+++ aliyun/ack/nodegroup/client.go
@@ -8,11 +8,14 @@ import (
   //"os"
   cs20151215  "github.com/alibabacloud-go/cs-20151215/v5/client"
   openapi  "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-  util  "github.com/alibabacloud-go/tea-utils/v2/service"
+  //util  "github.com/alibabacloud-go/tea-utils/v2/service"
   "github.com/alibabacloud-go/tea/tea"
 )
 
 
+type AliyunAckClient struct {
+	Client *cs20151215.Client
+}
 // Description:
 // 
 // 使用AK&SK初始化账号Client
@@ -20,7 +23,7 @@ import (
 // @return Client
 // 
 // @throws Exception
-func CreateClient(ak,sk,endpoint string) (result *cs20151215.Client, err error) {
+func NewAliyunAckClient(ak,sk,endpoint string) (*AliyunAckClient, error) {
   // 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考。
   // 建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378661.html。
   config := &openapi.Config{
@@ -31,8 +34,10 @@ func CreateClient(ak,sk,endpoint string) (result *cs20151215.Client, err error) 
   }
   // Endpoint 请参考 https://api.aliyun.com/product/CS
   config.Endpoint = tea.String(endpoint)
-  result = &cs20151215.Client{}
-  result, err = cs20151215.NewClient(config)
-  return result, err
+  result := &cs20151215.Client{}
+  result, err := cs20151215.NewClient(config)
+  return &AliyunAckClient{
+	Client: result,
+  },err 
 }
 
